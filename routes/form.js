9fc98804e7bd821f79a6be9aa77cacb0fcf3842c
uzair2210 <uzair27140@gmail.com>
2023-09-26 14:05:09 +0500

    vrj

--- routes/form.js
+++ routes/form.js
@@ -1,8 +1,37 @@
 const router = require('express').Router();
-const registrationModel = require('../models/users');
+const users = require('../models/users');
+const bcrypt = require('bcrypt')
+
+
+router.post("/register",async (req,res)=>{
+    try {
+
+        var hashedPassword = await bcrypt.hash(req.body.password,10)
+
+        var user = await UserModel.create({...req.body,password:hashedPassword})
+
+        res.json({
+            success:true,
+            message:user
+        })
+        
+        
+
+    } catch (error) {
+        res.json({
+            success:false,
+            message:"Something went wrong, Please try again later!"
+        })
+    }
+})
+
+
+
+
+
 
 router.get('/', async function (req, res) {
-    var students = await registrationModel.create(req.query);
+    var students = await users.create(req.query);
     res.send("<h1>Form Submission Successfully!</h1>.");
 });
 
