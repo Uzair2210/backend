dfcac5285acea07bccf003b31e21a0b590a4ec4d
uzair2210 <uzair27140@gmail.com>
2023-09-26 17:31:10 +0500

    ok

--- routes/form.js
+++ routes/form.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const registrationModel = require('../models/users');
 
 router.get('/', async function (req, res) {
-    var students = await users.create(req.query);
-    res.send("<h1>Form Submission Successfully!</h1>.");
+    var students = await registrationModel.create(req.query);
+    res.send("<h1>Form Submission successfull!</h1>");
 });
 
 
